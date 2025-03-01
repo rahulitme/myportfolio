7639d77b9d1b6cb483dbf2325556254c50de2e8e
Rahul <rahulmandal705071@gmail.com>
2025-03-01 18:37:27 +0530

    updater the color and resume

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,9 +6,8 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-links">
-          <button onClick={() => alert('Link 1 clicked')} className="footer-link">Link 1</button>
-          <button onClick={() => alert('Link 2 clicked')} className="footer-link">Link 2</button>
-          <button onClick={() => alert('Link 3 clicked')} className="footer-link">Link 3</button>
+          <button onClick={() => alert('https://github.com/rahulitme')} className="footer-link">GitHub</button>
+          <button onClick={() => alert('https://www.linkedin.com/in/rahul-kumar-191473256/')} className="footer-link">linkedin</button>
         </div>
         <div className="footer-copy">
           &copy; {new Date().getFullYear()} My Portfolio. All rights reserved.
